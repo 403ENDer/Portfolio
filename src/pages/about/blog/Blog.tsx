04674df622cee9807bfe1f502320ebe12bbb806c
Harish <harish11ndsh@gmail.com>
2025-03-25 19:17:34 +0530

    changes in blog

--- src/pages/about/blog/Blog.tsx
+++ src/pages/about/blog/Blog.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
 
-import { Box, Link } from "@chakra-ui/react";
+// import { Box, Link } from "@chakra-ui/react";
 
-import { ArticleTitle } from "pages/about/common/title/Title";
-import { configs } from "shared/content/Content";
+// import { ArticleTitle } from "pages/about/common/title/Title";
+// import { configs } from "shared/content/Content";
 
 export const Blog: FC = () => {
     return (
